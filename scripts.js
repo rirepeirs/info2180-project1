97558360b498378929b4e2cc01e9479825e815b5
Rianna Pearson <rirepeirs@gmail.com>
2022-10-30 23:33:44 -0500

    Added javascript to file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-/* Add your JavaScript to this file */
+
+
+
+document.getElementsByClassName("btn").onclick = function() {myFunction()};
+
+
+function myFunction() {
+    var x = document.getElementsByClassName("form-group");
+    var text = "";
+    var i;
+    for (i = 0; i < x.length ;i++) {
+      text += "Thank you! Your email address" + x.elements[i].value + "has been added to our mailing list";
+    }
+    alert(text);
+  }
+  
